f73f0bb96284ad5fc7103acca2af96ec904e4157
zion2200 <129466381+zion2200@users.noreply.github.com>
2025-10-10 16:53:32 +0900

    python06_lab

---
+++ python06_lab/class_score_predict.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    midterm_range = np.array([0, 125])
+    final_range = np.array([0, 100])
+
+    # Load score data
+    class_kr = np.loadtxt('data/class_score_kr.csv', delimiter=',')
+    class_en = np.loadtxt('data/class_score_en.csv', delimiter=',')
+    data = np.vstack((class_kr, class_en))
+
+    # Estimate a line: final = slope * midterm + y_intercept
+    line = [0, 0] # TODO) Find the best [slope, y_intercept] from 'data'
+
+    x = data[:, 0]                                # midterm
+    y = data[:, 1]                                # final
+    X = np.column_stack([x, np.ones_like(x)])     # design matrix [x, 1]
+    theta = np.linalg.pinv(X) @ y                 # [w, b] via pseudoinverse
+    line = [float(theta[0]), float(theta[1])]
+
+    # Predict scores
+    final = lambda midterm: line[0] * midterm + line[1]
+    while True:
+        try:
+            given = input('Q) Please input your midterm score (Enter or -1: exit)? ')
+            if given == '' or float(given) < 0:
+                break
+            print(f'A) Your final score is expected to be {final(float(given)):.3f}.')
+        except Exception as ex:
+            print(f'Cannot answer the question. (message: {ex})')
+            break
+
+    # Plot scores and the estimated line
+    plt.figure()
+    plt.plot(data[:,0], data[:,1], 'r.', label='The given data')
+    plt.plot(midterm_range, final(midterm_range), 'b-', label='Prediction')
+    plt.xlabel('Midterm scores')
+    plt.ylabel('Final scores')
+    plt.xlim(midterm_range)
+    plt.ylim(final_range)
+    plt.grid()
+    plt.legend()
+    plt.show()
